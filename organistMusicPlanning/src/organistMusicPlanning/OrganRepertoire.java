94f9cf6d83ab7003cd41a3fd9da85be750ce30ee
thanhtam.nguyen <thanhtam.nguyen@MU-C1-08.mullen.pvt.k12.co.us>
2024-10-23 10:57:40 -0600

    added more objects

--- organistMusicPlanning/src/organistMusicPlanning/OrganRepertoire.java
+++ organistMusicPlanning/src/organistMusicPlanning/OrganRepertoire.java
@@ -3,12 +3,16 @@ package organistMusicPlanning;
 public class OrganRepertoire
 	{
 		private String liturgical;
-		// Ordinary Time, Lent, Advent, Holy Week, Christmas, Easter
-		private int minutes;
+		// Ordinary Time, Lent, Advent, Holy Week, Christmas, Easter, Year-Round
+		private double minutesAndSeconds;
+		// 1.30, 2.45, 3, 4, 5
 		private String solo;
-		// Prelude, Communion, Postlude
+		// Prelude, Communion, Postlude, PreludeAndPostlude, PreludeAndCommunion
 		private String book;
 		// First Organ Book, Ultimate Organ Book, 8 Little Preludes and Fugues
 		private String composer;
+		// Bach, Gordon Young, Vierne, Buxtehude, Pachelbel
+		private String mood;
+		// Happy, Sad, Solemn, Joyful
 		
 	}
